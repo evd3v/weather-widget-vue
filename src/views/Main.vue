503b7e4aa1ebc150cc755da419d8bcdfb6c1e4e6
evd3v <evd3v@WIN-C9CG7DNGEFI>
2021-01-30 21:41:27 +0300

    create closed widget state

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,30 +1,36 @@
 <template>
-    <div class="widget">
-        <div class="widget-header">
-            <settings-outline-icon @click="settingsHandler" />
-            <close-solid-icon @click="closeHandler" />
+    <div class="widget-wrapper">
+        <div class="widget" v-if="widgetState">
+            <div class="widget-header">
+                <close-solid-icon @click="toggleWidgetState" />
+                <settings-outline-icon @click="settingsHandler" />
+            </div>
+            <div
+                class="swiper-wrapper"
+                v-if="!fetching && citiesWeather && citiesWeather.cnt"
+            >
+                <swiper :options="swiperOptions">
+                    <swiper-slide
+                        v-for="cityWeather in citiesWeather.list"
+                        :key="cityWeather.id"
+                    >
+                        <CityCard :city-weather="cityWeather" />
+                    </swiper-slide>
+                    <div class="swiper-pagination" slot="pagination"></div>
+                </swiper>
+            </div>
+            <template v-else>123</template>
         </div>
-        <div
-            class="swiper-wrapper"
-            v-if="!fetching && citiesWeather && citiesWeather.cnt"
-        >
-            <swiper :options="swiperOptions">
-                <swiper-slide
-                    v-for="cityWeather in citiesWeather.list"
-                    :key="cityWeather.id"
-                >
-                    <CityCard :city-weather="cityWeather" />
-                </swiper-slide>
-                <div class="swiper-pagination" slot="pagination"></div>
-            </swiper>
+        <div class="closed-widget" @click="toggleWidgetState" v-else>
+            <sun-with-cloud-color-icon />
         </div>
-        <template v-else>123</template>
     </div>
 </template>
 
 <script>
 import SettingsOutlineIcon from '@/assets/images/svg/settings-outline.svg?inline'
 import CloseSolidIcon from '@/assets/images/svg/close-solid.svg?inline'
+import SunWithCloudColorIcon from '@/assets/images/svg/sun-with-cloud-color.svg?inline'
 
 import { mapActions, mapState } from 'vuex'
 
@@ -32,10 +38,16 @@ import CityCard from '@/components/CityCard'
 
 export default {
     name: 'MainPage',
-    components: { CityCard, SettingsOutlineIcon, CloseSolidIcon },
+    components: {
+        CityCard,
+        SettingsOutlineIcon,
+        CloseSolidIcon,
+        SunWithCloudColorIcon
+    },
     data() {
         return {
             fetching: true,
+            widgetState: true,
             swiperOptions: {
                 pagination: {
                     el: '.swiper-pagination',
@@ -52,8 +64,8 @@ export default {
         settingsHandler() {
             console.log('on settings')
         },
-        closeHandler() {
-            console.log('on close')
+        toggleWidgetState() {
+            this.widgetState = !this.widgetState
         }
     },
     async mounted() {
@@ -65,54 +77,62 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.widget {
+.widget-wrapper {
     position: fixed;
     bottom: 30px;
     right: 30px;
-    overflow: hidden;
-    display: flex;
-    flex-direction: column;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    font-size: 14px;
-    width: 250px;
-    padding: 20px;
+    .widget {
+        overflow: hidden;
+        display: flex;
+        flex-direction: column;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        font-size: 14px;
+        width: 250px;
+        padding: 20px;
 
-    /* To prevent interact.js warnings */
-    user-select: none;
-    -ms-touch-action: none;
-    touch-action: none;
-
-    &:hover {
-        .widget-header {
-            visibility: visible;
-            opacity: 1;
+        &:hover {
+            .widget-header {
+                visibility: visible;
+                opacity: 1;
+            }
         }
-    }
 
-    &-header {
-        position: absolute;
-        right: 10px;
-        top: 5px;
-        height: 16px;
-        opacity: 0;
-        visibility: hidden;
-        transition: all ease-in-out 0.2s;
-        svg {
-            margin-right: 10px;
+        &-header {
+            position: absolute;
+            right: 5px;
+            top: 5px;
+            height: 52px;
+            opacity: 0;
+            visibility: hidden;
+            transition: all ease-in-out 0.2s;
+            display: flex;
+            flex-direction: column;
+            svg {
+                margin-top: 10px;
+                &:hover {
+                    cursor: pointer;
+                }
+            }
+        }
+
+        .swiper-wrapper {
             &:hover {
                 cursor: pointer;
             }
+
+            .swiper-pagination {
+                bottom: 0;
+            }
         }
     }
+    .closed-widget {
+        padding: 10px;
+        border: 1px solid #75d6ff;
+        border-radius: 50%;
 
-    .swiper-wrapper {
         &:hover {
             cursor: pointer;
-        }
-
-        .swiper-pagination {
-            bottom: 0;
         }
     }
 }
